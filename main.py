61b32a1bee909e2acfab566004763bf5990d12e4
The Minh Trinh <t.m.trinh@student.tudelf.nl>
2023-05-12 12:14:11 +0200

    Plot histogram and weibull estimation

--- main.py
+++ main.py
@@ -1,43 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.stats
+import scipy.stats as stats
 import datetime
-
-
-class Wind:
-
-    def __init__(self, data, start, end, label: str):
-        self.data = data.loc[(data['DTG'] >= start) & (data['DTG'] < end)]
-        self.label = label
-
-    def plot_histogram(self, height: str) -> None:
-        plt.hist(self.data[f"F{height}"], bins=list(range(0, 45)), density=True)
-        plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
-        plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
-        # plt.show()
-
-    def plot_velocities(self, height: str) -> None:
-        plt.plot(self.data["DTG"], self.data[f"F{height}"])
-        # plt.show()
-
-    def plot_velocityprofile(self) -> None:
-        altitude = [10, 20, 40, 60, 80, 100, 150, 200]
-        velocity = [np.percentile(self.data['F010'], 50), np.percentile(self.data['F020'],50), np.percentile(self.data['F040'],50),
-                    np.percentile(self.data['F060'], 50), np.percentile(self.data['F080'],50), np.percentile(self.data['F100'],50),
-                    np.percentile(self.data['F150'], 50), np.percentile(self.data['F200'],50)]
-        plt.plot(velocity, altitude, label=self.label)
-
-    def plot_velocityprofileV2(self) -> None:
-
-        altitude = [10, 20, 40, 60, 80, 100, 150, 200]
-        velocity = [self.data['F010'], self.data['F020'], self.data['F040'],
-                    self.data['F060'], self.data['F080'], self.data['F100'],
-                    self.data['F150'], self.data['F200']]
-
-        for veloc, alt in zip(velocity, altitude):
-            percentage = len(veloc[(3 <= veloc) & (veloc <= 25)]) / len(veloc)
-            print(f"for altitude {alt} the precentage is {percentage}")
 
 def main():
     head = ["DTG",
@@ -55,67 +20,8 @@ def main():
     f = pd.read_csv('Data/MMIJ_2004-2013.csv', delimiter='\t', comment='#', encoding='utf-8', header=None, names=head,
                     index_col=False, parse_dates=['DTG'], date_parser=dateparse)
     df = f.set_index('DTG')
-    # print(f.head())
 
-    # plt.hist(f['F010'], bins=list(range(0, 45)), density=True)
-    # plt.show()
-    #
-    # alt = [10,20,40,60,80,100,150,200]
-    # veloc = [np.average(f['F010']), np.average(f['F020']), np.average(f['F040']),
-    #          np.average(f['F060']), np.average(f['F080']), np.average(f['F100']),
-    #          np.average(f['F150']), np.average(f['F200'])]
-    #
-    # plt.plot(veloc, alt)
-    # plt.show()
-
-
-    '''Specify Season'''
-    # Spring
-    start_spring = pd.to_datetime('2004-03-20', format='%Y-%m-%d %H:%M:%S.%f')
-    end_spring = pd.to_datetime('2004-06-20', format='%Y-%m-%d %H:%M:%S.%f')
-
-    # Summer
-    start_summer = pd.to_datetime('2004-06-21', format='%Y-%m-%d %H:%M:%S.%f')
-    end_summer = pd.to_datetime('2004-09-23', format='%Y-%m-%d %H:%M:%S.%f')
-
-    # Fall
-    start_fall = pd.to_datetime('2004-09-23', format='%Y-%m-%d %H:%M:%S.%f')
-    end_fall = pd.to_datetime('2004-12-22', format='%Y-%m-%d %H:%M:%S.%f')
-
-    # Winter
-    start_winter = pd.to_datetime('2004-12-22', format='%Y-%m-%d %H:%M:%S.%f')
-    end_winter = pd.to_datetime('2005-03-20', format='%Y-%m-%d %H:%M:%S.%f')
-
-    spring = Wind(f, start_spring, end_spring, "spring")
-    summer = Wind(f, start_summer, end_summer, "summer")
-    fall = Wind(f, start_fall, end_fall, "fall")
-    winter = Wind(f, start_winter, end_winter, "winter")
-
-    spring.plot_histogram("010")
-    plt.show()
-    summer.plot_histogram("010")
-    plt.show()
-    fall.plot_histogram("010")
-    plt.show()
-    winter.plot_histogram("010")
-    plt.show()
-
-    # spring.plot_velocities("010")
-
-    spring.plot_velocityprofile()
-    summer.plot_velocityprofile()
-    fall.plot_velocityprofile()
-    winter.plot_velocityprofile()
-
-    spring.plot_velocityprofileV2()
-    summer.plot_velocityprofileV2()
-    fall.plot_velocityprofileV2()
-    winter.plot_velocityprofileV2()
-
-    plt.legend()
-    plt.show()
-
-
+    '''Specify seasons'''
     df_spring = df[((df.index.month == 3) & (df.index.day >= 20)) | (
                 (df.index.month == 4) | (df.index.month == 5) | ((df.index.month == 6) & (df.index.day < 21)))]
 
@@ -129,85 +35,68 @@ def main():
             (df.index.month == 1) | (df.index.month == 2) | ((df.index.month == 3) & (df.index.day < 20)))]
 
 
-    # Analysis of windspeed over time (year)
-    start_year = np.where((df.index.month == 1) & (df.index.day == 1) & (df.index.hour == 00) & (df.index.minute == 00))[0]
+    '''Define function to plot'''
+    def plot_hist_weibull_alt10(data):
+        alt = data['F010'].sort_values()
+        para1, para2, para3 = stats.weibull_min.fit(alt, floc=0)
+        alt.hist(alpha = 0.4, density = True, bins = 30)
+        plt.plot(alt, stats.weibull_min.pdf(alt, para1, para2, para3), color ='blue', label = '10m Altitude')
+        plt.axvline(np.median(alt), color='blue', linestyle='dashed', linewidth=1)
 
-    h_axis = []
-    average_f010 = []
-    average_f200 = []
+    def plot_hist_weibull_alt200(data):
+        alt = data['F200'].sort_values()
+        para1, para2, para3 = stats.weibull_min.fit(alt, floc=0)
+        alt.hist(alpha=0.4, density=True, bins=30)
+        plt.plot(alt, stats.weibull_min.pdf(alt, para1, para2, para3), color = 'red', label = '200m ALtitude')
+        plt.axvline(np.median(alt), color='red', linestyle='dashed', linewidth=1)
 
-    for year_idx in range(0, len(start_year) - 1):
-        average_f010.append(np.average((df.iloc[start_year[year_idx]:start_year[year_idx + 1]])['F010']))
-        average_f200.append(np.average((df.iloc[start_year[year_idx]:start_year[year_idx + 1]])['F200']))
-        h_axis.append(df.index[start_year[year_idx]])
+    def plot_hist_weibull_season(data):
+        plot_hist_weibull_alt10(data)
+        plot_hist_weibull_alt200(data)
+        plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
+        plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
+        plt.legend()
+        plt.show()
 
-    plt.plot(h_axis, average_f010)
-    plt.plot(h_axis, average_f200)
-    plt.show()
+    def plot_hist_weibull_alt010_2(data1, data2, data3, data4):
+        alt_010_1 = data1['F010'].sort_values()
+        para11, para21, para31 = stats.weibull_min.fit(alt_010_1, floc=0)
+        alt_010_2 = data2['F010'].sort_values()
+        para12, para22, para32 = stats.weibull_min.fit(alt_010_2, floc=0)
+        alt_010_3 = data3['F010'].sort_values()
+        para13, para23, para33 = stats.weibull_min.fit(alt_010_3, floc=0)
+        alt_010_4 = data4['F010'].sort_values()
+        para14, para24, para34 = stats.weibull_min.fit(alt_010_4, floc=0)
+        plt.plot(alt_010_1, stats.weibull_min.pdf(alt_010_1, para11, para21, para31))
+        plt.plot(alt_010_2, stats.weibull_min.pdf(alt_010_2, para12, para22, para32))
+        plt.plot(alt_010_3, stats.weibull_min.pdf(alt_010_3, para13, para23, para33))
+        plt.plot(alt_010_4, stats.weibull_min.pdf(alt_010_4, para14, para24, para34))
+        plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
+        plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
+        plt.show()
 
-    # Analysis of windspeed over time (month)
-    start_month = np.where((df.index.day == 1) & (df.index.hour == 00) & (df.index.minute == 00))[0]
+    def plot_hist_weibull_alt200_2(data1, data2, data3, data4):
+        alt_010_1 = data1['F200'].sort_values()
+        para11, para21, para31 = stats.weibull_min.fit(alt_010_1, floc=0)
+        alt_010_2 = data2['F200'].sort_values()
+        para12, para22, para32 = stats.weibull_min.fit(alt_010_2, floc=0)
+        alt_010_3 = data3['F200'].sort_values()
+        para13, para23, para33 = stats.weibull_min.fit(alt_010_3, floc=0)
+        alt_010_4 = data4['F200'].sort_values()
+        para14, para24, para34 = stats.weibull_min.fit(alt_010_4, floc=0)
+        plt.plot(alt_010_1, stats.weibull_min.pdf(alt_010_1, para11, para21, para31))
+        plt.plot(alt_010_2, stats.weibull_min.pdf(alt_010_2, para12, para22, para32))
+        plt.plot(alt_010_3, stats.weibull_min.pdf(alt_010_3, para13, para23, para33))
+        plt.plot(alt_010_4, stats.weibull_min.pdf(alt_010_4, para14, para24, para34))
+        plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
+        plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
+        plt.show()
 
-    h_axis = []
-    average_f010 = []
-    average_f200 = []
+    plot_hist_weibull_season(df_spring)
+    plot_hist_weibull_season(df_summer)
+    plot_hist_weibull_season(df_fall)
+    plot_hist_weibull_season(df_winter)
 
-    for month_idx in range(0, len(start_month) - 1):
-        average_f010.append(np.average((df.iloc[start_month[month_idx]:start_month[month_idx + 1]])['F010']))
-        average_f200.append(np.average((df.iloc[start_month[month_idx]:start_month[month_idx + 1]])['F200']))
-        h_axis.append(df.index[start_month[month_idx]])
-
-    plt.plot(h_axis, average_f010)
-    plt.plot(h_axis, average_f200)
-    plt.show()
-
-
-
-    # df.iloc[start_year]
-
-    # # Group the DataFrame by year and store in a dictionary
-    # df_spring_year = {year: group for year, group in df_spring.groupby(pd.Grouper(freq='Y'))}
-    # print(df_summer,'\n',df_fall,'\n', df_winter)
-
-    '''Same season different altitude in one plot'''
-    df_spring['F010'].hist(alpha = 0.4, density = True)
-    df_spring['F200'].hist(alpha = 0.4, density = True)
-    plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
-    plt.axvline(25, color='k', linestyle='dashed', linewidth=1) #Joshua Bogaert's
-    plt.show()
-
-    df_summer['F010'].hist(alpha = 0.4, density = True)
-    df_summer['F200'].hist(alpha = 0.4, density = True)
-    plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
-    plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
-    plt.show()
-
-    df_fall['F010'].hist(alpha = 0.4, density = True)
-    df_fall['F200'].hist(alpha = 0.4, density = True)
-    plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
-    plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
-    plt.show()
-
-    df_winter['F010'].hist(alpha = 0.4, density = True)
-    df_winter['F200'].hist(alpha = 0.4, density = True)
-    plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
-    plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
-    plt.show()
-
-    '''Same altitude different seasons in one plot'''
-    # xmin = 0
-    # xmax = 35
-    # x = np.linspace(xmin, xmax, 1000)
-    # y_spring_10 = scipy.stats.gaussian_kde(df_spring['F010'])
-    # y_summer_10 = scipy.stats.gaussian_kde(df_summer['F010'])
-    # y_fall_10 = scipy.stats.gaussian_kde(df_fall['F010'])
-    # y_winter_10 = scipy.stats.gaussian_kde(df_winter['F010'])
-    #
-    # plt.plot(y_spring_10)
-    # plt.plot(y_summer_10)
-    # plt.plot(y_fall_10)
-    # plt.plot(y_winter_10)
-    # plt.show()
 
 if __name__ == '__main__':
     main()
