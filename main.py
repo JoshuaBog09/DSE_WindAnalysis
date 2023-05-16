1896c0c7279cfdbfef3fedbb1c9d65b37c2f4284
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2023-05-16 23:05:33 +0200

    multiyear use of class to plot data

--- main.py
+++ main.py
@@ -50,11 +50,7 @@ class Wind:
 
         for veloc, alt in zip(velocity, altitude):
             percentage = len(veloc[(3 <= veloc) & (veloc <= 25)]) / len(veloc)
-            print(f"for altitude {alt} the percentage is {percentage}")
-
-    def plot_boxplot(self, height: str) -> None:
-        # self.data.boxplot(column=[f"F{height}"])
-        plt.boxplot(self.data[f"F{height}"])
+            print(f"{self.label}: for altitude {alt} the percentage is {percentage}")
 
     @staticmethod
     def plot_operationalwindow() -> None:
@@ -64,6 +60,7 @@ class Wind:
     @staticmethod
     def show_plots():
         plt.legend()
+        plt.grid()
         plt.show()
 
     @classmethod
@@ -166,6 +163,9 @@ def main_MultiYear():
                     index_col=False, parse_dates=['DTG'], date_parser=dateparse)
     df = f.set_index('DTG')
 
+    print(f"period average: {np.mean(df['F010'])}")
+    print(f"period average: {np.mean(df['F200'])}")
+
     df_spring = df[((df.index.month == 3) & (df.index.day >= 20)) | (
                 (df.index.month == 4) | (df.index.month == 5) | ((df.index.month == 6) & (df.index.day < 21)))]
 
@@ -178,38 +178,75 @@ def main_MultiYear():
     df_winter = df[((df.index.month == 12) & (df.index.day >= 22)) | (
             (df.index.month == 1) | (df.index.month == 2) | ((df.index.month == 3) & (df.index.day < 20)))]
 
+    print(f"spring average: {np.mean(df_spring['F010'])}")
+    print(f"spring average: {np.mean(df_spring['F200'])}")
+    print(f"summer average: {np.mean(df_summer['F010'])}")
+    print(f"summer average: {np.mean(df_summer['F200'])}")
+    print(f"fall average: {np.mean(df_fall['F010'])}")
+    print(f"fall average: {np.mean(df_fall['F200'])}")
+    print(f"winter average: {np.mean(df_winter['F010'])}")
+    print(f"winter average: {np.mean(df_winter['F200'])}")
 
-    # Analysis of windspeed over time (year)
-    start_year = np.where((df.index.month == 1) & (df.index.day == 1) & (df.index.hour == 00) & (df.index.minute == 00))[0]
+    period = Wind(df, "Full Period")
+    spring = Wind(df_spring, "spring")
+    summer = Wind(df_summer, "summer")
+    fall = Wind(df_fall, "fall")
+    winter = Wind(df_winter, "winter")
 
-    h_axis = []
-    average_f010 = []
-    average_f200 = []
+    seasons = [spring, summer, fall, winter, period]
 
-    for year_idx in range(0, len(start_year) - 1):
-        average_f010.append(np.average((df.iloc[start_year[year_idx]:start_year[year_idx + 1]])['F010']))
-        average_f200.append(np.average((df.iloc[start_year[year_idx]:start_year[year_idx + 1]])['F200']))
-        h_axis.append(df.index[start_year[year_idx]])
+    for season in seasons:
+        season.plot_histogram("010")
+        season.plot_weibull("010", "blue")
+        season.plot_histogram("200")
+        season.plot_weibull("200", "red")
+        season.plot_operationalwindow()
+        season.show_plots()
 
-    plt.plot(h_axis, average_f010)
-    plt.plot(h_axis, average_f200)
-    plt.show()
+    spring.plot_velocityprofile()
+    summer.plot_velocityprofile()
+    fall.plot_velocityprofile()
+    winter.plot_velocityprofile()
+    period.plot_velocityprofile()
+    Wind.show_plots()
 
-    # Analysis of windspeed over time (month)
-    start_month = np.where((df.index.day == 1) & (df.index.hour == 00) & (df.index.minute == 00))[0]
+    spring.plot_velocityprofileV2()
+    summer.plot_velocityprofileV2()
+    fall.plot_velocityprofileV2()
+    winter.plot_velocityprofileV2()
+    period.plot_velocityprofileV2()
 
-    h_axis = []
-    average_f010 = []
-    average_f200 = []
-
-    for month_idx in range(0, len(start_month) - 1):
-        average_f010.append(np.average((df.iloc[start_month[month_idx]:start_month[month_idx + 1]])['F010']))
-        average_f200.append(np.average((df.iloc[start_month[month_idx]:start_month[month_idx + 1]])['F200']))
-        h_axis.append(df.index[start_month[month_idx]])
-
-    plt.plot(h_axis, average_f010)
-    plt.plot(h_axis, average_f200)
-    plt.show()
+    # # Analysis of windspeed over time (year)
+    # start_year = np.where((df.index.month == 1) & (df.index.day == 1) & (df.index.hour == 00) & (df.index.minute == 00))[0]
+    #
+    # h_axis = []
+    # average_f010 = []
+    # average_f200 = []
+    #
+    # for year_idx in range(0, len(start_year) - 1):
+    #     average_f010.append(np.average((df.iloc[start_year[year_idx]:start_year[year_idx + 1]])['F010']))
+    #     average_f200.append(np.average((df.iloc[start_year[year_idx]:start_year[year_idx + 1]])['F200']))
+    #     h_axis.append(df.index[start_year[year_idx]])
+    #
+    # plt.plot(h_axis, average_f010)
+    # plt.plot(h_axis, average_f200)
+    # plt.show()
+    #
+    # # Analysis of windspeed over time (month)
+    # start_month = np.where((df.index.day == 1) & (df.index.hour == 00) & (df.index.minute == 00))[0]
+    #
+    # h_axis = []
+    # average_f010 = []
+    # average_f200 = []
+    #
+    # for month_idx in range(0, len(start_month) - 1):
+    #     average_f010.append(np.average((df.iloc[start_month[month_idx]:start_month[month_idx + 1]])['F010']))
+    #     average_f200.append(np.average((df.iloc[start_month[month_idx]:start_month[month_idx + 1]])['F200']))
+    #     h_axis.append(df.index[start_month[month_idx]])
+    #
+    # plt.plot(h_axis, average_f010)
+    # plt.plot(h_axis, average_f200)
+    # plt.show()
 
 
 
@@ -219,67 +256,67 @@ def main_MultiYear():
     # df_spring_year = {year: group for year, group in df_spring.groupby(pd.Grouper(freq='Y'))}
     # print(df_summer,'\n',df_fall,'\n', df_winter)
 
-    '''Define function to plot'''
-    def plot_hist_weibull_alt10(data):
-        alt = data['F010'].sort_values()
-        para1, para2, para3 = stats.weibull_min.fit(alt, floc=0)
-        alt.hist(alpha = 0.4, density = True, bins = 30)
-        plt.plot(alt, stats.weibull_min.pdf(alt, para1, para2, para3), color ='blue', label = '10m Altitude')
-        plt.axvline(np.median(alt), color='blue', linestyle='dashed', linewidth=1)
+    # '''Define function to plot'''
+    # def plot_hist_weibull_alt10(data):
+    #     alt = data['F010'].sort_values()
+    #     para1, para2, para3 = stats.weibull_min.fit(alt, floc=0)
+    #     alt.hist(alpha = 0.4, density = True, bins = 30)
+    #     plt.plot(alt, stats.weibull_min.pdf(alt, para1, para2, para3), color ='blue', label = '10m Altitude')
+    #     plt.axvline(np.median(alt), color='blue', linestyle='dashed', linewidth=1)
+    #
+    # def plot_hist_weibull_alt200(data):
+    #     alt = data['F200'].sort_values()
+    #     para1, para2, para3 = stats.weibull_min.fit(alt, floc=0)
+    #     alt.hist(alpha=0.4, density=True, bins=30)
+    #     plt.plot(alt, stats.weibull_min.pdf(alt, para1, para2, para3), color = 'red', label = '200m ALtitude')
+    #     plt.axvline(np.median(alt), color='red', linestyle='dashed', linewidth=1)
+    #
+    # def plot_hist_weibull_season(data):
+    #     plot_hist_weibull_alt10(data)
+    #     plot_hist_weibull_alt200(data)
+    #     plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
+    #     plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
+    #     plt.legend()
+    #     plt.show()
+    #
+    # def plot_hist_weibull_alt010_2(data1, data2, data3, data4):
+    #     alt_010_1 = data1['F010'].sort_values()
+    #     para11, para21, para31 = stats.weibull_min.fit(alt_010_1, floc=0)
+    #     alt_010_2 = data2['F010'].sort_values()
+    #     para12, para22, para32 = stats.weibull_min.fit(alt_010_2, floc=0)
+    #     alt_010_3 = data3['F010'].sort_values()
+    #     para13, para23, para33 = stats.weibull_min.fit(alt_010_3, floc=0)
+    #     alt_010_4 = data4['F010'].sort_values()
+    #     para14, para24, para34 = stats.weibull_min.fit(alt_010_4, floc=0)
+    #     plt.plot(alt_010_1, stats.weibull_min.pdf(alt_010_1, para11, para21, para31))
+    #     plt.plot(alt_010_2, stats.weibull_min.pdf(alt_010_2, para12, para22, para32))
+    #     plt.plot(alt_010_3, stats.weibull_min.pdf(alt_010_3, para13, para23, para33))
+    #     plt.plot(alt_010_4, stats.weibull_min.pdf(alt_010_4, para14, para24, para34))
+    #     plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
+    #     plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
+    #     plt.show()
+    #
+    # def plot_hist_weibull_alt200_2(data1, data2, data3, data4):
+    #     alt_010_1 = data1['F200'].sort_values()
+    #     para11, para21, para31 = stats.weibull_min.fit(alt_010_1, floc=0)
+    #     alt_010_2 = data2['F200'].sort_values()
+    #     para12, para22, para32 = stats.weibull_min.fit(alt_010_2, floc=0)
+    #     alt_010_3 = data3['F200'].sort_values()
+    #     para13, para23, para33 = stats.weibull_min.fit(alt_010_3, floc=0)
+    #     alt_010_4 = data4['F200'].sort_values()
+    #     para14, para24, para34 = stats.weibull_min.fit(alt_010_4, floc=0)
+    #     plt.plot(alt_010_1, stats.weibull_min.pdf(alt_010_1, para11, para21, para31))
+    #     plt.plot(alt_010_2, stats.weibull_min.pdf(alt_010_2, para12, para22, para32))
+    #     plt.plot(alt_010_3, stats.weibull_min.pdf(alt_010_3, para13, para23, para33))
+    #     plt.plot(alt_010_4, stats.weibull_min.pdf(alt_010_4, para14, para24, para34))
+    #     plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
+    #     plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
+    #     plt.show()
 
-    def plot_hist_weibull_alt200(data):
-        alt = data['F200'].sort_values()
-        para1, para2, para3 = stats.weibull_min.fit(alt, floc=0)
-        alt.hist(alpha=0.4, density=True, bins=30)
-        plt.plot(alt, stats.weibull_min.pdf(alt, para1, para2, para3), color = 'red', label = '200m ALtitude')
-        plt.axvline(np.median(alt), color='red', linestyle='dashed', linewidth=1)
-
-    def plot_hist_weibull_season(data):
-        plot_hist_weibull_alt10(data)
-        plot_hist_weibull_alt200(data)
-        plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
-        plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
-        plt.legend()
-        plt.show()
-
-    def plot_hist_weibull_alt010_2(data1, data2, data3, data4):
-        alt_010_1 = data1['F010'].sort_values()
-        para11, para21, para31 = stats.weibull_min.fit(alt_010_1, floc=0)
-        alt_010_2 = data2['F010'].sort_values()
-        para12, para22, para32 = stats.weibull_min.fit(alt_010_2, floc=0)
-        alt_010_3 = data3['F010'].sort_values()
-        para13, para23, para33 = stats.weibull_min.fit(alt_010_3, floc=0)
-        alt_010_4 = data4['F010'].sort_values()
-        para14, para24, para34 = stats.weibull_min.fit(alt_010_4, floc=0)
-        plt.plot(alt_010_1, stats.weibull_min.pdf(alt_010_1, para11, para21, para31))
-        plt.plot(alt_010_2, stats.weibull_min.pdf(alt_010_2, para12, para22, para32))
-        plt.plot(alt_010_3, stats.weibull_min.pdf(alt_010_3, para13, para23, para33))
-        plt.plot(alt_010_4, stats.weibull_min.pdf(alt_010_4, para14, para24, para34))
-        plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
-        plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
-        plt.show()
-
-    def plot_hist_weibull_alt200_2(data1, data2, data3, data4):
-        alt_010_1 = data1['F200'].sort_values()
-        para11, para21, para31 = stats.weibull_min.fit(alt_010_1, floc=0)
-        alt_010_2 = data2['F200'].sort_values()
-        para12, para22, para32 = stats.weibull_min.fit(alt_010_2, floc=0)
-        alt_010_3 = data3['F200'].sort_values()
-        para13, para23, para33 = stats.weibull_min.fit(alt_010_3, floc=0)
-        alt_010_4 = data4['F200'].sort_values()
-        para14, para24, para34 = stats.weibull_min.fit(alt_010_4, floc=0)
-        plt.plot(alt_010_1, stats.weibull_min.pdf(alt_010_1, para11, para21, para31))
-        plt.plot(alt_010_2, stats.weibull_min.pdf(alt_010_2, para12, para22, para32))
-        plt.plot(alt_010_3, stats.weibull_min.pdf(alt_010_3, para13, para23, para33))
-        plt.plot(alt_010_4, stats.weibull_min.pdf(alt_010_4, para14, para24, para34))
-        plt.axvline(3, color='k', linestyle='dashed', linewidth=1)
-        plt.axvline(25, color='k', linestyle='dashed', linewidth=1)
-        plt.show()
-
-    plot_hist_weibull_season(df_spring)
-    plot_hist_weibull_season(df_summer)
-    plot_hist_weibull_season(df_fall)
-    plot_hist_weibull_season(df_winter)
+    # plot_hist_weibull_season(df_spring)
+    # plot_hist_weibull_season(df_summer)
+    # plot_hist_weibull_season(df_fall)
+    # plot_hist_weibull_season(df_winter)
 
     # 4 seasons Box plots F010
     new = pd.DataFrame([df_spring["F010"], df_summer["F010"], df_fall["F010"], df_winter["F010"]]).transpose()
